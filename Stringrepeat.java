5a380f3c7a68795fd2ed49974f3ac9dbd49895ec
Aparna <aparnaishwarya.s2022csbs@sece.ac.in>
2025-12-08 15:18:50 +0530

    added

---
+++ Stringrepeat.java
@@ -0,0 +1,24 @@
+
+public class stringRepeat {
+    public static void main(String[]args){
+        String s1 = "asd" ; 
+        String s2 = "asd" ; 
+        // System.out.println(s2.compareTo(s1));
+
+        String str = "Ap" ; 
+        int n = 3 ; 
+        String ans = "" ; 
+        if(str.length()>3){
+            String sub = str.substring(0,3) ; 
+            for(int i=0;i<n;i++){
+                ans+=sub ; 
+            }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                ans+=str ; 
+            }
+        }
+        System.out.println(ans);
+    }
+}
